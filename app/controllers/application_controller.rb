e6dd733446336dfdf91a1a723b773ea9435040c0
xttebrocx <samueldcorbett@uchicago.edu>
2022-04-23 00:46:32 +0000

    Taking a break for the weekend, need to implement the APIs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+
+  def nothing_fancy
+    render({:template => "homepage/dashboard.html.erb"})
+  end
+
+  def foreign_exchange
+    render({:template => "external/forex.html.erb"})
+  end 
+
+  def covid 
+    render({:template => "external/covid.html.erb"})
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+
+
+  get("/", :controller => "application", :action => "nothing_fancy")
+
+  get("/forex", :controller => "application", :action => "foreign_exchange")
+  
+  get("/covid", :controller => "application", :action => "covid")
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
