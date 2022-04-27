5ec6f5e986daaa64ae963953098e992bb6e8c08a
xttebrocx <samueldcorbett@uchicago.edu>
2022-04-27 20:25:21 +0000

    Completed, 100%

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,44 @@ class ApplicationController < ActionController::Base
     render({:template => "homepage/dashboard.html.erb"})
   end
 
-  def foreign_exchange
+  def first_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @keys = @symbols_hash.keys
+    @array_of_symbols = @keys
+    
     render({:template => "external/forex.html.erb"})
   end 
+
+  def second_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @keys = @symbols_hash.keys
+    @array_of_symbols = @keys
+    @symbol = params.fetch("symbol")
+
+    render({:template => "external/conversion_selection.html.erb"})
+  end
+
+  def conversion_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @keys = @symbols_hash.keys
+    @array_of_symbols = @keys
+    @symbol = params.fetch("symbol")
+    @symbol2 = params.fetch("symbol2")
+
+    @rate_data = open("https://api.exchangerate.host/convert?from="+@symbol+"&to="+@symbol2).read
+    @parsed_rate = JSON.parse(@rate_data)
+    @rate_hash = @parsed_rate.fetch("info")
+    @true_rate = @rate_hash.fetch("rate")
+    
+    
+    render({:template => "external/conversion.html.erb"})
+  end
 
   def covid 
     render({:template => "external/covid.html.erb"})
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   get("/", :controller => "application", :action => "nothing_fancy")
 
-  get("/forex", :controller => "application", :action => "foreign_exchange")
+  get("/forex", :controller => "application", :action => "first_currency")
+  
+  get("/forex/:symbol", :controller => "application", :action => "second_currency")
+  
+  get("/forex/:symbol/:symbol2", :controller => "application", :action => "conversion_currency")
   
   get("/covid", :controller => "application", :action => "covid")
 
